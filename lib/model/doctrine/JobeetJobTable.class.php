c6ec6f30d45d27f94b11dcfb9bd1b953791f2056
Bill <Billythakidd04@gmail.com>
2014-06-11 22:53:47 -0400

    added getActiveJobs()

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -17,12 +17,17 @@ class JobeetJobTable extends Doctrine_Table
         return Doctrine_Core::getTable('JobeetJob');
     }
     
-    public function getActiveJobs() {
-        $q = Doctrine_Query::create()
-            ->from('JobeetJob j')
-            ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()))
-            ->orderBy('j.expires_at DESC');
-      
+    public function getActiveJobs(Doctrine_Query $q = null)
+    {
+        if (is_null($q))
+        {
+          $q = Doctrine_Query::create()
+            ->from('JobeetJob j');
+        }
+
+        $q->andWhere('j.expires_at > ?', date('Y-m-d H:i:s', time()))
+          ->addOrderBy('j.expires_at DESC');
+
         return $q->execute();
     }
 }
