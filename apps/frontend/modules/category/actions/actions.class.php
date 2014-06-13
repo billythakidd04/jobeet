6e7e83631639b13cf6b3e535c80bf0fe2e1bc104
Bill Caffery <Billythakidd04@gmail.com>
2014-06-13 15:42:11 -0400

    category changes pre rebuild add sluggable

--- apps/frontend/modules/category/actions/actions.class.php
+++ apps/frontend/modules/category/actions/actions.class.php
@@ -1,22 +1,8 @@
 <?php
-
-/**
- * category actions.
- *
- * @package    jobeet
- * @subpackage category
- * @author     Your name here
- * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
- */
 class categoryActions extends sfActions
 {
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
+  public function executeShow(sfWebRequest $request)
   {
-    $this->forward('default', 'module');
+    $this->category = $this->getRoute()->getObject();
   }
 }
